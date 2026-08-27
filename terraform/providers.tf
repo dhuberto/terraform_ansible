ad0e69842b194624c6f4dd77ce8e9005f90b178a
Dhuberto <dhuberto@gmail.com>
2026-08-27 10:24:28 -0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     bucket         = "danilo-terraform-backend-2026"
     key            = "terraform_ansible/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
